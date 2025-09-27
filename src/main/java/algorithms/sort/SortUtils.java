95d537213c789962c95140dd10806e0370e8920c
Madiyar <wolfenstrun@gmail.com>
2025-09-27 21:52:01 +0500

    • refactor(util): partition, swap, shuffle, guards

---
+++ src/main/java/algorithms/sort/SortUtils.java
@@ -0,0 +1,37 @@
+package algorithms.sort;
+
+import java.util.Random;
+
+public class SortUtils {
+    private static final Random random = new Random();
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+    public static boolean isNullOrEmpty(int[] arr) {
+        return arr == null || arr.length == 0;
+    }
+}
